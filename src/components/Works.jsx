4bf9d68b8f6e8b68ae14d392fe9e28483546724f
ThomasLaspas <makislaspas@gmail.com>
2024-05-09 09:38:53 +0300

    small device

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-
+import { useMediaQuery } from "react-responsive";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
@@ -20,6 +20,65 @@ const ProjectCard = ({
   source_code_link2,
   language,
 }) => {
+  const isSmallDevice = useMediaQuery({ query: "(max-width: 640px)" });
+
+  if (isSmallDevice) {
+    return (
+      <div className="bg-tertiary p-5 rounded-2xl sm:w-[320px]">
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+          <div className="absolute inset-0 flex justify-start m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link2, "_blank")}
+              className="bg-white w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={image2}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">
+            {language ? descriptiongr : description}
+          </p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
@@ -86,6 +145,38 @@ const ProjectCard = ({
 };
 
 const Works = ({ language }) => {
+  const isSmallDevice = useMediaQuery({ query: "(max-width: 640px)" });
+
+  if (isSmallDevice) {
+    return (
+      <>
+        <p className={`${styles.sectionSubText} `}>
+          {language ? "Προσωπική δουλειά" : "My work"}
+        </p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+
+        <div className="w-full flex">
+          <div className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+            {language
+              ? "Τα παρακάτω έργα αναδεικνύουν τις δεξιότητές μου και την εμπειρία μου με πραγματικά παραδείγματα από τη δουλειά μου. Κάθε έργο περιγράφεται συνοπτικά και περιλαμβάνει συνδέσμους προς αποθετήρια κώδικα και ζωντανές επίδειξεις, αναδεικνύοντας την ικανότητά μου να λύνω πολύπλοκα προβλήματα, να χειρίζομαι επιδέξια διάφορες τεχνολογίες και να διαχειρίζομαι αποτελεσματικά έργα."
+              : "The projects below highlight my skills and experience with real-world examples of my work. Each project is succinctly described and includes links to code repositories and live demos, showcasing my ability to solve intricate problems, adeptly handle various technologies, and efficiently manage projects."}
+          </div>
+        </div>
+
+        <div className={`mt-20 flex flex-wrap gap-7`}>
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={`project-${index}`}
+              index={index}
+              {...project}
+              language={language}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -106,7 +197,7 @@ const Works = ({ language }) => {
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className={`mt-20 flex flex-wrap gap-7`}>
         {projects.map((project, index) => (
           <ProjectCard
             key={`project-${index}`}
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
   const ref = useRef();
   const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(9000), { radius: 1.2 })
+    random.inSphere(new Float32Array(1500), { radius: 1.2 })
   );
 
   useFrame((state, delta) => {
@@ -20,7 +20,7 @@ const Stars = (props) => {
         <PointMaterial
           transparent
           color="#f272c8"
-          size={0.001}
+          size={0.003}
           sizeAttenuation={true}
           depthWrite={false}
         />
