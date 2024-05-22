6d8dbbefd873d01992dc568c0672ac2ba42a19f3
ThomasLaspas <makislaspas@gmail.com>
2024-05-22 10:04:30 +0300

    again links

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -33,15 +33,14 @@ const ProjectCard = ({
           />
 
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover cursor-pointer">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={github}
-                alt="source code"
-                className="w-1/2 h-1/2 object-contain"
-              />
+            <div className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+              <a href={source_code_link}>
+                <img
+                  src={github}
+                  alt="source code"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </a>
             </div>
           </div>
           <div className="absolute inset-0 flex justify-start m-3 card-img_hover">
@@ -49,11 +48,13 @@ const ProjectCard = ({
               onClick={() => window.open(source_code_link2, "_blank")}
               className="bg-white w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
-              <img
-                src={image2}
-                alt="source code"
-                className="w-1/2 h-1/2 object-contain"
-              />
+              <a href={source_code_link2}>
+                <img
+                  src={image2}
+                  alt="source code"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </a>
             </div>
           </div>
         </div>
